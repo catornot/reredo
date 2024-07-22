861c845757cb2198cc93a5663aac6027a97a69ba
catornot <41955154+catornot@users.noreply.github.com>
2024-07-22 15:34:33 +0700

    initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
-// Bevy code commonly triggers these lints and they may be important signals
-// about code quality. They are sometimes hard to avoid though, and the CI
-// workflow treats them as errors, so this allows them throughout the project.
-// Feel free to delete this line.
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
 use bevy::asset::AssetMetaCheck;
+use bevy::log::{Level, LogPlugin};
 use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(AssetPlugin {
-            // Wasm builds will check for meta files (that don't exist) if this isn't set.
-            // This causes errors and even panics in web builds on itch.
-            // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
-            meta_check: AssetMetaCheck::Never,
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(AssetPlugin {
+                    // Wasm builds will check for meta files (that don't exist) if this isn't set.
+                    // This causes errors and even panics in web builds on itch.
+                    // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+                    meta_check: AssetMetaCheck::Never,
+                    ..default()
+                })
+                .set(LogPlugin {
+                    level: Level::WARN,
+                    filter: "wgpu=off,bevy_render=info,bevy_ecs=trace".to_string(),
+                    custom_layer: |_| None,
+                }),
+        )
         .add_systems(Startup, setup)
         .run();
 }
